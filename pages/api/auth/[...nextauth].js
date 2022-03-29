635e0907686f27449f49e58ca173f59a5d2ab414
Tawnee <allprt89@gmail.com>
2022-03-29 12:16:18 -0600

    comment code

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,33 +3,41 @@ import Providers from 'next-auth/providers'
 import { verifyPassword } from '../../../lib/auth'
 import { connectToDatabase } from '../../../lib/db'
 
+// [...nextauth].js catches all routes not otherwise specified in the api/auth folder
+
 export default NextAuth({
 	session: {
 		jwt: true,
 	},
+	// providers are services that can be used to sign in a user
 	providers: [
+		// The Credentials provider allows you to handle signing in with arbitrary credentials, such as a username and password, two-factor authentication or hardware device
 		Providers.Credentials({
 			async authorize(credentials) {
+				// connect to mongodb and users collection, tries to find a user with the supplied email
 				const client = await connectToDatabase()
-
 				const usersCollection = client.db().collection('users')
 				const user = await usersCollection.findOne({ email: credentials.email })
 
+				// error if there isn't a user with that email
 				if (!user) {
 					client.close()
 					throw new Error('No user found')
 				}
 
+				// checks if supplied password can be hashed to match the saved password
 				const isValid = await verifyPassword(
 					credentials.password,
 					user.password
 				)
 
+				// error if incorrect password
 				if (!isValid) {
 					client.close()
 					throw new Error('Could not login')
 				}
 
+				// if email and password match return email. this will be visible on the front end, so don't return password
 				client.close()
 				return { email: user.email }
 			},
--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -2,12 +2,15 @@ import { connectToDatabase } from '../../../lib/db'
 import { hashPassword } from '../../../lib/auth'
 
 async function handler(req, res) {
+	// exits if request isn't a POST request
 	if (req.method !== 'POST') {
 		return
 	}
 
+	// get email and password from the request
 	const { email, password } = req.body
 
+	// sends error status if email or password aren't valid
 	if (
 		!email ||
 		!email.includes('@') ||
@@ -21,11 +24,13 @@ async function handler(req, res) {
 		return
 	}
 
+	// connect to mongodb and tries to find existing email
 	const client = await connectToDatabase()
 	const db = client.db()
 
 	const existingUser = await db.collection('users').findOne({ email })
 
+	// sends error status if trying to sign up with an existing email
 	if (existingUser) {
 		res.status(422).json({
 			message: 'Email already in use',
@@ -34,13 +39,16 @@ async function handler(req, res) {
 		return
 	}
 
+	// hashes supplied password
 	const hashedPassword = await hashPassword(password)
 
+	// adds email and password to database
 	const result = await db.collection('users').insertOne({
 		email,
 		password: hashedPassword,
 	})
 
+	// sends success status and closes db connection after adding user
 	res.status(201).json({ message: 'Signed up successfully!' })
 	client.close()
 }

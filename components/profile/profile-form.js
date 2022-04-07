49be927ba0bb7ea76a5aa8431cf6b2b8b84b8c78
Tawnee <allprt89@gmail.com>
2022-04-07 12:58:41 -0600

    add password change functionality

--- components/profile/profile-form.js
+++ components/profile/profile-form.js
@@ -1,21 +1,34 @@
-import classes from './profile-form.module.css';
+import { useRef } from 'react'
+import classes from './profile-form.module.css'
 
-function ProfileForm() {
-  return (
-    <form className={classes.form}>
-      <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
-      </div>
-      <div className={classes.control}>
-        <label htmlFor='old-password'>Old Password</label>
-        <input type='password' id='old-password' />
-      </div>
-      <div className={classes.action}>
-        <button>Change Password</button>
-      </div>
-    </form>
-  );
+function ProfileForm({ onChangePassword }) {
+	const oldPasswordRef = useRef()
+	const newPasswordRef = useRef()
+
+	function submitHandler(event) {
+		event.preventDefault()
+
+		const oldPassword = oldPasswordRef.current.value
+		const newPassword = newPasswordRef.current.value
+
+		onChangePassword({ oldPassword, newPassword })
+	}
+
+	return (
+		<form className={classes.form} onSubmit={submitHandler}>
+			<div className={classes.control}>
+				<label htmlFor='new-password'>New Password</label>
+				<input type='password' id='new-password' ref={newPasswordRef} />
+			</div>
+			<div className={classes.control}>
+				<label htmlFor='old-password'>Old Password</label>
+				<input type='password' id='old-password' ref={oldPasswordRef} />
+			</div>
+			<div className={classes.action}>
+				<button>Change Password</button>
+			</div>
+		</form>
+	)
 }
 
-export default ProfileForm;
+export default ProfileForm
